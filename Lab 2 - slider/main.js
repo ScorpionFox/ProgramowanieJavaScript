4e5b356f5ee64b71803d2819b844b8bbd37dc9ac
ScorpionFox <93731860+ScorpionFox@users.noreply.github.com>
2023-11-09 09:55:54 +0100

    Add files via upload

--- Lab 2 - slider/main.js
+++ Lab 2 - slider/main.js
@@ -1,23 +1,23 @@
-// notatnik z zajęć
+// // notatnik z zajęć
 
-const main = document.querySelector('main')
+// const main = document.querySelector('main')
 
-// jednorazowe wykonanie kodu po określonym czasie
-const timeoutRef = setTimeout(
-    () => {
-        main.innerHTML = 'Msg from setTimeout'
-    },
-    2000
-)
+// // jednorazowe wykonanie kodu po określonym czasie
+// const timeoutRef = setTimeout(
+//     () => {
+//         main.innerHTML = 'Msg from setTimeout'
+//     },
+//     2000
+// )
 
-// wykonywanie kodu co określony czas
-let licznik = 0
-const intervalRef = setInterval(
-    () => {
-        main.innerHTML = `Msg from setInterval: ${licznik++}`
-    },
-    4000
-)
+// // wykonywanie kodu co określony czas
+// let licznik = 0
+// const intervalRef = setInterval(
+//     () => {
+//         main.innerHTML = `Msg from setInterval: ${licznik++}`
+//     },
+//     4000
+// )
 
 // kasujemy setInterval
 // clearInterval(intervalRef)
@@ -27,3 +27,31 @@ const intervalRef = setInterval(
 
 
 // window.requestAnimationFrame
+
+let counter = 1;
+setInterval(function () {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 6) {
+        counter = 1;
+    }
+}, 5000);
+
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+prevBtn.addEventListener('click', () => {
+    counter--;
+    if (counter < 1) {
+        counter = 6;
+    }
+    document.getElementById('radio' + counter).checked = true;
+});
+
+nextBtn.addEventListener('click', () => {
+    counter++;
+    if (counter > 6) {
+        counter = 1;
+    }
+    document.getElementById('radio' + counter).checked = true;
+})
