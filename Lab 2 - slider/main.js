f1a0e0a267d4acc6c460bcd98fa859db31ed7bf7
ScorpionFox <93731860+ScorpionFox@users.noreply.github.com>
2024-01-13 16:24:02 +0100

    Done

--- Lab 2 - slider/main.js
+++ Lab 2 - slider/main.js
@@ -1,51 +1,34 @@
-// // notatnik z zajęć
-
-// const main = document.querySelector('main')
-
-// // jednorazowe wykonanie kodu po określonym czasie
-// const timeoutRef = setTimeout(
-//     () => {
-//         main.innerHTML = 'Msg from setTimeout'
-//     },
-//     2000
-// )
-
-// // wykonywanie kodu co określony czas
-// let licznik = 0
-// const intervalRef = setInterval(
-//     () => {
-//         main.innerHTML = `Msg from setInterval: ${licznik++}`
-//     },
-//     4000
-// )
-
-// kasujemy setInterval
-// clearInterval(intervalRef)
-
-// kasujemy setTimeout
-// clearTimeout(intervalRef)
-
-
-// window.requestAnimationFrame
-
 let counter = 1;
-setInterval(function () {
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if (counter > 6) {
-        counter = 1;
-    }
-}, 5000);
+let isPaused = false;
+const intervalTime = 5000; // Czas dla automatycznego przewijania
 
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+const playPauseBtn = document.querySelector('#pauseBtn');
+
+function togglePlayPause() {
+    isPaused = !isPaused;
+    playPauseBtn.textContent = isPaused ? 'Wznów' : 'Pauza';
+}
+
+function changeSlide() {
+    if (!isPaused) {
+        document.querySelector('#radio' + counter).checked = true;
+        counter++;
+        if (counter > 6) {
+            counter = 1;
+        }
+    }
+}
+
+const sliderInterval = setInterval(changeSlide, intervalTime);
 
 prevBtn.addEventListener('click', () => {
     counter--;
     if (counter < 1) {
         counter = 6;
     }
-    document.getElementById('radio' + counter).checked = true;
+    document.querySelector('#radio' + counter).checked = true;
 });
 
 nextBtn.addEventListener('click', () => {
@@ -53,5 +36,5 @@ nextBtn.addEventListener('click', () => {
     if (counter > 6) {
         counter = 1;
     }
-    document.getElementById('radio' + counter).checked = true;
-})
+    document.querySelector('#radio' + counter).checked = true;
+});
